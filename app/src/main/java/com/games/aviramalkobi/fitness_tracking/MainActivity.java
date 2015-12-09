9c4d1c6b69ca157b611a02ecef29756c88c50738
Aviram alkobi <aviramalkobi@gmail.com>
2015-12-09 11:31:13 +0200

    sign is
    test

--- app/src/main/java/com/games/aviramalkobi/fitness_tracking/MainActivity.java
+++ app/src/main/java/com/games/aviramalkobi/fitness_tracking/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.Sign_in);
+        setContentView(R.layout.sign_in);
     }
 
     @Override
