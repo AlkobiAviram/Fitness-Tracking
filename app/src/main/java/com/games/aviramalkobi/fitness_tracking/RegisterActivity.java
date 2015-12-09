3cee4690dd3389946dc4474f8499ffff3115dfc5
Aviram alkobi <aviramalkobi@gmail.com>
2015-12-09 15:41:16 +0200

    transaction between Sign in and Register

--- app/src/main/java/com/games/aviramalkobi/fitness_tracking/RegisterActivity.java
+++ app/src/main/java/com/games/aviramalkobi/fitness_tracking/RegisterActivity.java
@@ -4,32 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sign_in);
-
-        Button SignInButton = (Button)findViewById(R.id.Register_button);
-        SignInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                    Intent intent = new Intent(MainActivity.this , RegisterActivity.class);
-                    startActivity(intent);
-
-            }
-        });
+        setContentView(R.layout.register_activity);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_register_ctivity, menu);
         return true;
     }
 
